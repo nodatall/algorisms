67bbceae4727096951b99cdd347a8d362030b3c7
Sylvan Swierkosz <sswierkosz@gmail.com>
2017-05-23 16:05:53 -0700

    Completes printAllSubArraysWith0Sum

--- src/printAllSubArraysWith0Sum/index.js
+++ src/printAllSubArraysWith0Sum/index.js
@@ -1,1 +1,61 @@
-export default () => {}
+const findSets = (num, array) => {
+  if ( num === 1 ) {
+    return array.map( (value, index) => {
+      return [{ index, value }]
+    })
+  }
+  let result = []
+  findSets(num - 1, array).forEach( set => {
+    const index = set[set.length - 1].index
+
+    for ( let i = index + 1; i < array.length; i++ ) {
+      result.push(set.concat({
+        index: i,
+        value: array[i]
+      }))
+    }
+  })
+  return result
+}
+
+const isAlreadyIn = (set, setBunch) =>
+  setBunch.reduce( (acc, compareSet) => {
+    if ( acc ) {
+      return true
+    }
+    if ( compareSet.length !== set.length ) {
+      return acc
+    }
+    for ( let i = 0; i < compareSet.length; i++ ) {
+      if ( compareSet[i] !== set[i] ) {
+        return false
+      }
+    }
+    return true
+  }, false)
+
+export default array => {
+  let allTheSets = []
+
+  for ( let i = 1; i <= array.length; i++ ) {
+    allTheSets = [...findSets(i, array), ...allTheSets]
+  }
+
+  const result = allTheSets.reduce( (acc, set) => {
+    const setInfo = set.reduce( (setAcc, numberObject) => {
+      setAcc.sum += numberObject.value
+      setAcc.set.push( numberObject.value )
+      return setAcc
+    }, {sum: 0, set: []})
+    if ( setInfo.sum === 0 ) {
+      if ( isAlreadyIn(setInfo.set, acc ) ) {
+        return acc
+      }
+      return [...acc, setInfo.set]
+    } else {
+      return acc
+    }
+  },[])
+
+  return result
+}
--- src/printAllSubArraysWith0Sum/readMe.md
+++ src/printAllSubArraysWith0Sum/readMe.md
@@ -22,9 +22,25 @@ returns
 
 ##### Solution in English
 
+Find all subsets of length n - 1
+->  for each subset,
+go through all remaining number after index of last number in subset
+-> concatenate into subsets
+Check every set to see if it meets 0 sum condition
+-> if so, save in result
+
+##### Brainstorming
+
+1,2 | 1,0 | 1,3 | 2,0 | 2,3 | 0,3
+
+1,2,0 | 1,2,3 | 1,0,3 | 2,0,3
+
+1,2,0,3
+
+Combinatorics, binomial coefficient (combination formula):
+nCr = n! / (n - r)! * r!
+
 ##### Time complexity
 
 ---
 Based on [this](http://www.techiedelight.com/find-sub-array-with-0-sum/)
-
-[-3, -1, 2, 4]
